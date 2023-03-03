ccd877d57813aaba3489ccbe888d057b0743d05b
Rokralio <rjgonzalez.1996@gmail.com>
2023-03-03 13:39:24 -0500

    add charsDuck a nuestro project

---
+++ src/redux/charsDuck.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+//const
+let initialData = {
+  fetching: false,
+  array: [],
+  current: {}
+}
+
+let URL = "https://rickandmortyapi.com/api/character"
+
+
+let GET_CHARACTERS = 'GET_CHARACTERS'
+let GET_CHARACTERS_SUCCESS = 'GET_CHARACTERS_SUCCESS'
+let GET_CHARACTERS_ERROR = 'GET_CHARACTERS_ERROR'
+
+//Reducer
+export default function reducer (state = initialData, action) {
+  switch (action.type) {
+    case GET_CHARACTERS:
+      return { ...state, fetching: true}
+    case GET_CHARACTERS_ERROR:
+      return {...state, fetching: false, error: action.payload}
+    case GET_CHARACTERS_SUCCESS:
+      return { ...state, array: action.payload, fetching: false }
+    default:
+      return state
+  }
+}
+//actions (thunks)
+export let getCharactersAction = () => (dispatch, getState) =>{
+  dispatch({
+    type: GET_CHARACTERS,
+    
+  })
+  return axios.get(URL) //axios va a pedir los personajes
+  .then(res =>{
+    dispatch({
+      type: GET_CHARACTERS_SUCCESS,
+      payload: res.data.results //payload es la dada.
+      })
+  })
+  .catch(err =>{
+    dispatch({
+      type: GET_CHARACTERS_ERROR,
+      payload: err.response.message
+    })
+  })
+}
+
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,22 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import userReducer from "./userDuck";
+import charsReducer,{getCharactersAction} from "./charsDuck";
 import thunk from "redux-thunk";//para hacer promesas, consumos al backend
 
 //herramientas de desarrolador
 let rootreducer = combineReducers({
   user: userReducer,
+  characters: charsReducer
 })
 //pregunta si tiene instalado o soporta las herramientas de desarrollo || o si usas compose
 const composeEnancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function generateStore (){
-  let store = createStore(rootreducer,
-    composeEnancers(applyMiddleware(thunk))
-  )
-return store;
+    let store = createStore(
+      rootreducer,
+      composeEnancers(applyMiddleware(thunk))
+    )
+  //consiguiendo los personajes por primera vez
+  getCharactersAction()(store.dispatch, store.getState)
+  return store
 }
